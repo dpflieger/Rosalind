316b766103dc765523ed121a18ce4337b1025c21
David Pflieger <pflieger.dav@gmail.com>
2016-10-31 16:59:05 +0100

    Overlap Graphs
    with itertools tee() and product() functions

--- 11_GRPH/GRPH.py
+++ 11_GRPH/GRPH.py
@@ -1,13 +1,11 @@
 from Bio import SeqIO
+from itertools import product, tee
 
-fasta_file = "rosalind_grph.txt"
+records_a, records_b = tee(SeqIO.parse("rosalind_grph.txt", "fasta"))
 
-with open("solution.txt", "w") as out:
-    for record_a in SeqIO.parse(fasta_file, "fasta"):
-        for record_b in SeqIO.parse(fasta_file, "fasta"):
-            if record_a.seq == record_b.seq:
-                pass
-            if record_a.seq[-3:] == record_b.seq[:3]:
-                print(record_a.id, record_b.id, file=out)
-            if record_a.seq[3:] == record_b.seq[-3:]:
-                print(record_a.id, record_b.id, file=out)
+with open("grph_output.txt", "w") as out:
+    for record_a, record_b in product(records_a, records_b):
+        if record_a.seq[-3:] == record_b.seq[:3]:
+            print(record_a.id, record_b.id, file=out)
+        if record_a.seq[3:] == record_b.seq[-3:]:
+            print(record_a.id, record_b.id, file=out)
