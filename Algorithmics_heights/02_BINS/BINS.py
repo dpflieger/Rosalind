2eb8f4fda956c585f9c2c3859a66a2ce2e325262
David Pflieger <pflieger.dav@gmail.com>
2016-11-07 19:08:09 +0100

    Binary Search

--- Algorithmics_heights/02_BINS/BINS.py
+++ Algorithmics_heights/02_BINS/BINS.py
@@ -1,13 +1,11 @@
 from bisect import bisect_left
-import numpy as np
 
-def binary_search(a, x, lo=0, hi=None):   # can't use a to specify default for hi
-    hi = hi if hi is not None else len(a) # hi defaults to len(a)
-    pos = bisect_left(a, x, lo, hi)          # find insertion position
-    return (pos if pos != hi and a[pos] == x else -2) # don't walk off the end
+def binary_search(a, x, lo=0, hi=None):
+    hi = hi if hi is not None else len(a)
+    pos = bisect_left(a, x, lo, hi)
+    return (pos if pos != hi and a[pos] == x else -2)
 
 infile = "rosalind_bins.txt"
-#infile = "rosalind_sample.txt"
 sorted_array = [*map(int, open(infile).readlines()[2].split())]
 list_integers = [*map(int, open(infile).readlines()[3].split())]
 
